605ba899b18557b44ba647e501bd31223b49f1c3
osama <osamaeslam2013@gmail.com>
2023-10-07 17:16:04 +0300

    this is the blackAndWhite filter

---
+++ blackAndWhite.cpp
@@ -0,0 +1,43 @@
+//================================== Osama Eslam ======================================
+void blackAndWhite()
+{
+  // first calculate average gray level
+  int number = 0;
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < SIZE; j++)
+    {
+      number += image[i][j];
+    }
+  }
+  number /= (SIZE * SIZE); // to get the average
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < SIZE; j++)
+    {
+      // the pixel greater than average be white
+      if (image[i][j] > number)
+      {
+        image[i][j] = 255;
+      }
+      // the pixel lower than the average be black
+      else
+      {
+        image[i][j] = 0;
+      }
+    }
+  }
+}
+//==================================2nd Filter======================================
+void invertImage()
+{
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < SIZE; j++)
+    {
+      // Invert the pixel value by subtracting it from 255
+      image[i][j] = 255 - image[i][j];
+    }
+  }
+}
+//================================== Osama Eslam ======================================
