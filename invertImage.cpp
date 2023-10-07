d6fa81008acb068a4c39ca4a13ac90d98e2ae474
Seif Khaled <seifk359869@gmail.com>
2023-10-07 15:15:05 +0300

    invertImage Function

--- invertImage.cpp
+++ invertImage.cpp
@@ -2,7 +2,7 @@
 void invertImage() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            // Invert the pixel value by subtracting it from 255
+            //Invert the pixel value by subtracting it from 255
             image[i][j] = 255 - image[i][j];
         }
     }
