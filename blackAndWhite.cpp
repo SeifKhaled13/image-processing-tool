f4c717739bdd0dafadf48dc4275e2171460b88a2
osama <osamaeslam2013@gmail.com>
2023-10-07 17:17:36 +0300

    this is blackAndWhite filter

--- blackAndWhite.cpp
+++ blackAndWhite.cpp
@@ -28,16 +28,4 @@ void blackAndWhite()
     }
   }
 }
-//==================================2nd Filter======================================
-void invertImage()
-{
-  for (int i = 0; i < SIZE; i++)
-  {
-    for (int j = 0; j < SIZE; j++)
-    {
-      // Invert the pixel value by subtracting it from 255
-      image[i][j] = 255 - image[i][j];
-    }
-  }
-}
 //================================== Osama Eslam ======================================
